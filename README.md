fa6ddbb0cca6ae49e15c9d8ad736493cc701103c
Henrik Adamski <h.adamski@live.de>
2019-06-29 14:04:53 +0200

    Update Readme and enable dark mode

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ This application is running on Undertow as web server.
 
 - Java 11
 - Spring Boot 2.1.6.RELEASE
-- Vaadin Flow 13.0.8
+- Vaadin Flow 13.0.9
 
 ## Run this example
 
--- src/main/java/com/avides/vaadin/ui/MainLayout.java
+++ src/main/java/com/avides/vaadin/ui/MainLayout.java
@@ -7,7 +7,7 @@ import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
 @Push
-@Theme(Lumo.class)
+@Theme(value = Lumo.class, variant = Lumo.DARK)
 public class MainLayout extends Div implements RouterLayout
 {
     private static final long serialVersionUID = -218890586070440330L;
