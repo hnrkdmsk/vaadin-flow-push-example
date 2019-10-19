4a8b07aac12b7d2e7cd6ad45f052fd4859a6f12b
Henrik Adamski <h.adamski@live.de>
2019-10-19 23:18:36 +0200

    Add test

---
+++ src/test/java/com/avides/vaadin/ApplicationIT.java
@@ -0,0 +1,19 @@
+package com.avides.vaadin;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringBootTest(classes = Application.class, webEnvironment = RANDOM_PORT)
+@SpringJUnitConfig
+class ApplicationIT
+{
+    @Test
+    void testRun()
+    {
+        // nothing ..
+        // simulates the start up process
+    }
+}
