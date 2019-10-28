c06a46a057444de8b4f13e0179fe7422c11b446e
Henrik Adamski <hadamski@avides.com>
2019-10-28 15:54:47 +0100

    Test with docker

---
+++ src/main/resources/db/migration/V1__schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `test` (
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
--- src/test/java/com/avides/vaadin/ApplicationTest.java
+++ src/test/java/com/avides/vaadin/ApplicationTest.java
@@ -2,18 +2,30 @@ package com.avides.vaadin;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = Application.class, webEnvironment = RANDOM_PORT)
-@SpringJUnitConfig
+@ActiveProfiles("it")
 class ApplicationTest
 {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
     void testRun()
     {
         // nothing ..
         // simulates the start up process
+
+        // TODO: remove this after action tests
+        jdbcTemplate.update("INSERT INTO test (name) VALUES ('Test User')");
+        var result = jdbcTemplate.queryForList("SELECT name FROM test", String.class);
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals("Test User", result.get(0));
     }
 }
